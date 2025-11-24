1deb7a2908932c50d7f92201fbe6fbca32daafc8
shaye <sm0548540902@gmail.com>
2025-11-24 09:32:56 +0200

    finish

--- courses_orm.py
+++ courses_orm.py
@@ -1,30 +1,28 @@
-from dataclasses import field
+
 from typing import Optional
-from sqlmodel import create_engine,Session,select
-from sqlalchemy import table
-from sqlmodel import SQLModel, Field
+
+from sqlmodel import SQLModel, Field,create_engine,Session,select
 class Course(SQLModel,table = True):
     id:Optional[int] = Field(default=None,primary_key=True)
     name:str
     hours:int
     is_active:bool = True
-    engine = create_engine("sqlite:///courses.db,echo = True")
-    @staticmethod
-    def create_db_and_tables():
-        SQLModel.metadata.create_all(Course.engine)
+engine = create_engine("sqlite:///courses.db")
 
-    def add_course(self,name,hours,is_active = True):
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def add_course(name,hours,is_active = True):
         course = Course(name = name,hours = hours,is_active = is_active)
-        with Session(self.engine) as session:
+        with Session(engine) as session:
             session.add(course)
             session.commit()
             session.refresh(course)
         print(f"added course with id = {course.id}")
-    @staticmethod
-    def get_active_courses():
-        pass
-        # with Session(Course.engine) as session:
-            # statement = select(Course).where(Course.is_active == True)
-            # result = session.exec(statement)
-            # courses = result.all()
-            # return courses
+
+def get_active_courses():
+        with Session(engine) as session:
+            statement = select(Course).where(Course.is_active == True)
+            result = session.exec(statement)
+            courses = result.all()
+            return courses
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from courses_orm import *
 if __name__ == "__main__":
-    Course.create_db_and_tables()
-    Course.add_course("sql basic",20,True)
-    Course.add_course("Python Intro", 30, True)
-    Course.add_course("Legacy System", 10, False)
-    active = Course.get_active_courses()
+    create_db_and_tables()
+    add_course("sql basic",20,True)
+    add_course("Python Intro", 30, True)
+    add_course("Legacy System", 10, True)
+    active = get_active_courses()
     print("active courses")
     for c in active:
         print(f"{c.id}: {c.name} ({c.hours} hours)")
